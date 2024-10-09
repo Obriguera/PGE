b56e339052c20244644c0659297208d1a7a60a91
Octavio Briguera <obriguera03@gmail.com>
2024-10-09 16:08:17 -0300

    Borrar Cosas Viejas

--- Tarea 1/Pasajero.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pasajero.h"
-#include <iostream>
-using namespace std;
-
-Pasajero::Pasajero(string nombre, string apellido, string DNI) : nombre(nombre), apellido(apellido), DNI(DNI) {}
-
-void Pasajero::mostrarInformacion() const {
-    cout << "Nombre: " << nombre << ", Apellido: " << apellido << ", DNI: " << DNI << endl;
-}
--- pge-2-v1/ClaseProdYCarrit.cpp
+++
@@ -1,7 +0,0 @@
-// ClaseProdYCarrit.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-using namespace std;
-
-
